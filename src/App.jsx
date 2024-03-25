da116d591a20042a41eaefd30c7ae7bfb10c4d43
yutteee <yusaku_nakamura_aa@icloud.com>
2024-03-25 19:53:19 +0900

    落下速度を調整

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
     Events.on(engine.current, "collisionStart", collisionHandler);
 
     // 重力を変更
-    engine.current.world.gravity.y = 0.3;
+    engine.current.world.gravity.y = 0.5;
 
     return () => {
       Render.stop(render);
